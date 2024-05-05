6db8b7400909c35a4d803ed943db3e6b2a3b5ff9
stelatoris <porchpu@gmail.com>
2024-05-05 17:43:10 +0200

    1st commit

---
+++ controllers/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// require 'functions.php';
+
+$heading = "Home";
+
+
+require "views/index.view.php";
---
+++ views/partials/head.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-gray-100">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <!-- <link rel="stylesheet" href="app.css"> -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="h-full">
+
+    <!--
+  This example requires updating your template:
+
+  ```
+  <html class="h-full bg-gray-100">
+  <body class="h-full">
+  ```
+-->
+    <div class="min-h-full">
