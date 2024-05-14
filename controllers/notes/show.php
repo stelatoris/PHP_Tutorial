728b388eeaae9f356c770d7769f5af7688972902
stelatoris <porchpu@gmail.com>
2024-05-14 21:21:03 +0200

    Refactored Router

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -1,22 +1,41 @@
 <?php
+
 use Core\Database;
 use Core\Response;
 
-$config = require base_path('config.php'); 
-
+$config = require base_path('config.php');
 $db = new Database($config['database']);
 
 $currentUserId = 2; //user id hard coded for testing
 
 $id = $_GET['id'];
 
-$note = $db->query('select * from notes where id = :id', [
-    'id' => $id
-])->findOrFail();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-authorize($note['user_id'] != $currentUserId, $status = Response::FORBIDDEN);
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $id
+    ])->findOrFail();
 
-view("notes/show.view.php", [
-    'heading' => 'Note',
-    'note' => $note
-]);
+    authorize($note['user_id'] != $currentUserId, $status = Response::FORBIDDEN);
+
+
+    $db->query('delete from notes where id = :id', [
+        'id' => $id
+    ]);
+
+    header('location: /notes');
+    exit();
+} else {
+
+
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $id
+    ])->findOrFail();
+
+    authorize($note['user_id'] != $currentUserId, $status = Response::FORBIDDEN);
+
+    view("notes/show.view.php", [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
